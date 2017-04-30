b11257d041b996836b7ea20f12929e909ef0a387
Stephen C. Phillips <steve@scphillips.com>
2017-04-30 17:12:18 +0100

    Refactors Morse.js off website into first attempt at Node.js package

---
+++ Morse.js
@@ -0,0 +1,219 @@
+// This code is © Copyright Stephen C. Phillips, 2013-2016.
+// Email: steve@scphillips.com
+
+if(typeof(String.prototype.trim) === "undefined") {
+  String.prototype.trim = function() {
+    return String(this).replace(/^\s+|\s+$/g, '');
+  };
+}
+// function log(message) {
+//   if(typeof console == "object"){
+//     console.log(message);
+//   }
+// }
+
+Morse = function() {
+  var DITS_PER_WORD = 50;  // based on "PARIS "
+  var text2morseH = {
+    'A': ".-",
+    'B': "-...",
+    'C': "-.-.",
+    'D': "-..",
+    'E': ".",
+    'F': "..-.",
+    'G': "--.",
+    'H': "....",
+    'I': "..",
+    'J': ".---",
+    'K': "-.-",
+    'L': ".-..",
+    'M': "--",
+    'N': "-.",
+    'O': "---",
+    'P': ".--.",
+    'Q': "--.-",
+    'R': ".-.",
+    'S': "...",
+    'T': "-",
+    'U': "..-",
+    'V': "...-",
+    'W': ".--",
+    'X': "-..-",
+    'Y': "-.--",
+    'Z': "--..",
+    '1': ".----",
+    '2': "..---",
+    '3': "...--",
+    '4': "....-",
+    '5': ".....",
+    '6': "-....",
+    '7': "--...",
+    '8': "---..",
+    '9': "----.",
+    '0': "-----",
+    '.': ".-.-.-",
+    ',': "--..--",
+    ':': "---...",
+    '?': "..--..",
+    '\'': ".----.",
+    '-': "-....-",
+    '/': "-..-.",
+    '(': "-.--.-",
+    ')': "-.--.-",
+    '"': ".-..-.",
+    '@': ".--.-.",
+    '=': "-...-",
+    ' ': "/" //Not morse but helps translation
+  };
+  var morse2textH = {};
+  var prosign2morseH = {
+    '<AA>': '.-.-',
+    '<AR>': '.-.-.',
+    '<AS>': '.-...',
+    '<BK>': '-...-.-',
+    '<BT>': '-...-',  // also <TV>
+    '<CL>': '-.-..-..',
+    '<CT>': '-.-.-',
+    '<DO>': '-..---',
+    '<KN>': '-.--.',
+    '<SK>': '...-.-',  // also <VA>
+    '<VA>': '...-.-',
+    '<SN>': '...-.',  // also <VE>
+    '<VE>': '...-.',
+    '<SOS>': '...---...'
+  };
+  var morsepro2textH = {};
+  var text2morseproH = {};
+  for (var text in text2morseH) {
+    text2morseproH[text] = text2morseH[text];
+    morse2textH[text2morseH[text]] = text;
+    morsepro2textH[text2morseH[text]] = text;
+  }
+  for (var sign in prosign2morseH) {
+    text2morseproH[sign] = prosign2morseH[sign];
+    morsepro2textH[prosign2morseH[sign]] = sign;
+  }
+};
+
+Morse.prototype = {
+
+  constructor: Morse,
+
+  tidyText: function(text) {
+    text = text.toUpperCase();
+    text = text.trim();
+    text = text.replace(/\s+/g, ' ');
+    return text;
+  },
+
+  text2morse: function(text, useProsigns) {
+    if (typeof useProsigns === "undefined") { useProsigns = true; }
+
+    text = Morse.tidyText(text);
+    var ret = {
+      morse: "",
+      message: "",
+      hasError: false
+    };
+    if (text === "") {
+      return ret;
+    }
+
+    var tokens = [];
+    var prosign;
+    var token_length;
+    while (text.length > 0) {
+      token_length = 1;
+      if (useProsigns) {
+        prosign = text.match(/^<...?>/);  // array of matches
+        if (prosign) {
+          token_length = prosign[0].length;
+        }
+      }
+      tokens.push(text.slice(0, token_length));
+      text = text.slice(token_length, text.length);
+    }
+    var dict;
+    if (useProsigns) {
+      dict = Morse.text2morseproH;
+    } else {
+      dict = Morse.text2morseH;
+    }
+    var i, c, t;
+    for (i = 0; i < tokens.length; i++) {
+      t = tokens[i];
+      c = dict[t];
+      if (c === undefined) {
+        ret.message += "#" + t + "#";
+        ret.morse += "# ";
+        ret.hasError = true;
+      } else {
+        ret.message += t;
+        ret.morse += c + " ";
+      }
+    }
+    ret.morse = ret.morse.slice(0, ret.morse.length - 1);
+    return ret;
+  },
+
+  tidyMorse: function(morse) {
+    morse = morse.trim();
+    morse = morse.replace(/\|/g, "/");  // unify the word separator
+    morse = morse.replace(/\//g, " / ");  // make sure word separators are spaced out
+    morse = morse.replace(/\s+/g, " ");  // squash multiple spaces into single spaces
+    morse = morse.replace(/(\/ )+\//g, "/");  // squash multiple word separators
+    //morse = morse.replace(/^ \/ /, "");  // remove initial word separators
+    //morse = morse.replace(/ \/ $/, "");  // remove trailing word separators
+    morse = morse.replace(/^\s+/, "");
+    morse = morse.replace(/\s+$/, "");
+    morse = morse.replace(/_/g, "-");  // unify the dash character
+    return morse;
+  },
+
+  morse2text: function(morse, useProsigns) {
+    if (typeof useProsigns === "undefined") { useProsigns = true; }
+
+    morse = Morse.tidyMorse(morse);
+    var ret = {
+      morse: "",
+      message: "",
+      hasError: false
+    };
+    if (morse === "") {
+      return ret;
+    }
+
+    var tokens = morse.split(" ");
+    var dict;
+    if (useProsigns) {
+      dict = Morse.morsepro2textH;
+    } else {
+      dict = Morse.morse2textH;
+    }
+    var c, t;
+    for (var i = 0; i < tokens.length; i++) {
+      t = tokens[i];
+      c = dict[t];
+      if (c === undefined) {
+        ret.morse += "#" + t + "# ";
+        ret.message += "#";
+        ret.hasError = true;
+      } else {
+        ret.morse += t + " ";
+        ret.message += c;
+      }
+    }
+    ret.morse = ret.morse.slice(0, ret.morse.length - 1);
+    return ret;
+  },
+
+  isMorse: function(input) {
+    // perhaps should be called "looksLikeMorse"?!
+    input = Morse.tidyMorse(input);
+    if (input.match(/^[ /.-]*$/)) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+};
