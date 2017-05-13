b3ff7747b5f140b50dccdb315b4c4bd5b9b0ce40
Stephen C. Phillips <steve@scphillips.com>
2017-05-13 13:53:16 +0100

    Corrects Farnsworth speed calculation

--- morse-pro-cw.js
+++ morse-pro-cw.js
@@ -54,7 +54,18 @@ MorseCW.prototype = {
     */
     getTimings: function() {
         var dit = 60000 / (this.DITS_PER_WORD * this.wpm);  // 60000 is 1 minute in milliseconds
-        var r = this.wpm / this.fwpm;  // slow down the spaces by this ratio
+        // "PARIS " is 31 units for the characters and 19 units for the inter-character spaces and inter-word space
+        // One unit takes 1 * 60 / (50 * wpm)
+        // The 31 units should take 31 * 60 / (50 * wpm)  seconds at wpm
+        // PARIS should take 50 * 60 / (50 * fpm) to transmit at fpm, or 60 / fwpm  seconds at fwpm
+        // Keeping the time for the characters constant,
+        // The spaces need to take: (60 / fwpm) - [31 * 60 / (50 * wpm)] seconds in total
+        // The spaces are 4 inter-character spaces of 3 units and 1 inter-word space of 7 units. Their ratio must be maintained.
+        // A space unit is: [(60 / fwpm) - [31 * 60 / (50 * wpm)]] / 19 seconds
+        // Comparing that to  60 / (50 * wpm)  gives a ratio of (50.wpm - 31.fwpm) / 19.fwpm
+        var SPACES_IN_PARIS = 19;
+        // slow down the spaces by this ratio
+        var r = (this.DITS_PER_WORD * this.wpm - (this.DITS_PER_WORD - SPACES_IN_PARIS) * this.fwpm) / (SPACES_IN_PARIS * this.fwpm);
         return this.getTimingsGeneral(dit, 3 * dit, dit, 3 * dit * r, 7 * dit * r);
     },
 
