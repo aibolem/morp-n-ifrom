eb5442136728b629aeb498389ddc3143e1388a27
Stephen C Phillips <steve@scphillips.com>
2022-03-04 13:23:28 +0000

    Adds more dictionaries

--- src/dictionary/index.js
+++ src/dictionary/index.js
@@ -1,9 +1,15 @@
 import * as international from "./international.js";
 import * as american from "./american.js";
 import * as cyrillic from "./cyrillic.js";
+import * as cyrillicUkraine from "./cyrillic-ukraine.js";
+import * as hebrew from "./hebrew.js";
+import * as arabic from "./arabic.js";
 
 export var dictionaries = {};
 
 dictionaries.international = international.dictionary;
 dictionaries.american = american.dictionary;
 dictionaries.cyrillic = cyrillic.dictionary;
+dictionaries.cyrillicUkraine = cyrillicUkraine.dictionary;
+dictionaries.hebrew = hebrew.dictionary;
+dictionaries.arabic = arabic.dictionary;
